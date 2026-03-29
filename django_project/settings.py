cea16b8f247620d1a596325ba44cfe6831ee5433
Vitaliy <ryashentsev.vitaliy@gmail.com>
2026-03-29 10:54:10 +0300

    pro: implement WhiteNoise for high-perfomance static file serving

--- django_project/settings.py
+++ django_project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -126,6 +127,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 MEDIA_ROOT = BASE_DIR / "media"
@@ -135,3 +138,12 @@ MEDIA_URL = "media/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 INTERNAL_IPS = ["127.0.0.1"]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
